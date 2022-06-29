fb28681badf71788e4b473cf856257190959e588
RoniDeringer <ronideringer@gmail.com>
2022-06-29 20:04:35 -0300

    push git (29/06/22)

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Triângulo de Sierpinski
+
+
+Trabalho Realizado para a matéria de `Matemática Discreta` BCC - IFC Rio do Sul
+
+[Artigo sobre](https://docs.google.com/document/d/1TA9zTFYzBGFAc7lp5cRu9v_7_YznCvNOQxn5lnsRxUo/edit?usp=sharing)
---
+++ triangulo_sierpinski.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+numero_recursao = int(input('Informe o número de interações: '))
+L1 = np.array([0, 0])
+L2 = np.array([2, 0])
+L3 = np.array([1, np.sqrt(3)])
+ponto_atual = np.array([0, 0])
+
+def mid_point(p1,p2):
+    ponto_medio = (p1 + p2) / 2
+    return ponto_medio
+    
+i=0
+
+def funcaoRecursiva(ponto_atual,N,i):
+  i+=1
+  if (N>=i):
+   
+    rn = np.random.randint(1, 4)
+    if (rn == 1):
+      ponto_atual = mid_point(ponto_atual, L1)
+    if (rn == 2):
+      ponto_atual = mid_point(ponto_atual, L2)
+    if (rn == 3):
+      ponto_atual = mid_point(ponto_atual, L3)
+     
+    print(i)
+    plt.plot(ponto_atual[0], ponto_atual[1], '.')
+    
+    funcaoRecursiva(ponto_atual,N,i) 
+  
+  return "fim"
+
+funcaoRecursiva(ponto_atual,numero_recursao,x)
